143e7650dd10eabccc9c1756ae6970d667629683
strongbad03 <joshuat@Dell-Lappie.localdomain>
2020-12-08 21:49:33 -0600

    adding tightvnc, setting up print test

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-# Ubuntu only
-# install Git and Python
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.6 -y
-sudo apt install git -y
-sudo apt install firefox -y
+## Ubuntu only
+## install Git and Python
+#echo "Installing Python..."
+#sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt update
+#sudo apt install python3.6 -y
+#sudo apt install firefox -y
+#echo "Python installed. Configuring Git..."
 
 # Git config
-ssh-keygen -t ed25519 -C "strongbad03"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed25519.pub
-echo "===================
-COPY SSH KEY UP TO GITHUB NOW
-==================="
-sleep 30
-git clone git@github.com:strongbad03/streetlingo.git
+#ssh-keygen -t ed25519 -b 2048 -t rsa -f /home/ubuntu/.ssh/id_ed25519 -q -N ""
+#eval "$(ssh-agent -s)"
+#ssh-add ~/.ssh/id_ed25519
+#cat ~/.ssh/id_ed25519.pub
+#echo "===================
+#COPY SSH KEY UP TO GITHUB NOW
+#==================="
+#sleep 30
+#touch ~/.ssh/known_hosts
+#ssh-keygen -F github.com || ssh-keyscan github.com >>~/.ssh/known_hosts
+#git clone git@github.com:strongbad03/streetlingo.git
+#cd streetlingo
+#sudo chmod 700 bootstrap.sh
+#echo "Repo cloned, starting boostrap."
+#./bootstrap.sh
 
 # install Pip, virtualenv, and create venv
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -26,6 +33,16 @@ python3.6 -m pip install virtualenv
 python3.6 -m virtualenv streetlingo ~/venvs/streetlingo/
 source venv/streetlingo/bin/activate
 pip install -r requirements.txt
+python3.6 main.py
+
+# TightVNC
+sudo apt install ubuntu-desktop -y
+sudo apt install tightvncserver -y
+sudo apt install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal -y
+vncserver :1
+\cp ./xstartup ~/.vnc/xstartup
+vncserver -kill :1
+vncserver :1
 
 # install QGIS
 sudo apt install gnupg software-properties-common
--- main.py
+++ main.py
@@ -12,9 +12,7 @@ test_text = "影響包含對氣候的變化以及自然資源的枯竭程度"
 if __name__ == "__main__":
     # fetch_data(url) # download streets geodatabase
     # unzip_data(archive)
-    df = load_data(file)
+    # df = load_data(file)
     # print(lid_text(test_text))
-    ddf = dask_geopandas.from_geopandas(df, npartitions=4)
-    result = da.lid_text(df.x)
-    resul
-
+    # ddf = dask_geopandas.from_geopandas(df, npartitions=4)
+    print(lid_text(test_text))
